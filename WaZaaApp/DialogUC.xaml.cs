01a3a52aff2a5823f29fe394e5b22fffcda28708
Pashuskui Volodumur <pashunskyi@gmail.com>
2020-06-25 18:05:43 +0300

    перенесення проекту з .net framework на .net core

---
+++ WaZaaApp/DialogUC.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WaZaaApp
+{
+    /// <summary>
+    /// Interaction logic for DialogUC.xaml
+    /// </summary>
+    public partial class DialogUC : UserControl
+    {
+        public DialogUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
